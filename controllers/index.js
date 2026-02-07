c23e0df938790efab23569c3fdf5cb686df679ab
klee2187 <kkelly6@byui.edu>
2026-02-06 21:48:04 -0700

    removed duplicates

--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-const getName = (req, res) => {
-  res.send('Sarah Birch');
-};
-
-module.exports = { getName };
--- routes/temple.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const temples = require('../controllers/temple.js');
-
-router.get('/', temples.findAll);
-router.get('/:temple_id', temples.findOne);
-
-router.post('/', temples.create);
-
-module.exports = router;
